16af6273da3d4d4f8600ca6adf94ca26c20a1e39
PiotrSadolewski <piotr.sadolewski00@gmail.com>
2023-08-01 13:03:16 +0200

    added getTodos() from api

--- src/app/todo/services/todo.service.ts
+++ src/app/todo/services/todo.service.ts
@@ -1,52 +1,47 @@
 import { Injectable } from '@angular/core';
 import { Todo } from '../models/todo';
+import { Observable, map } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
   private todoId = 1;
-  private todoList: Todo[] = [
-    {
-      id: this.todoId++,
-      title: 'serve the app',
-      completed: true,
-    },
-    {
-      id: this.todoId++,
-      title: 'familiarise yourself with the codebase',
-      completed: false,
-    },
-    {
-      id: this.todoId++,
-      title: 'start talking to the api',
-      completed: false,
-    },
-  ];
 
-  // TODO replace with a get request
-  todos: Promise<Todo[]> = Promise.resolve(this.todoList);
+  constructor(private readonly http: HttpClient) {
 
-  async addTodo(title: string): Promise<Todo> {
-    // TODO: replace with a POST request
-    const todo = {
-      id: this.todoId++,
-      title: title,
-      completed: false,
-    };
-    this.todoList.push(todo);
-
-    return todo;
   }
 
-  async updateTodo(updatedTodo: Todo): Promise<Todo> {
-    // TODO: replace with a PUT request
-    const foundTodo = this.todoList.find((todo) => todo.id === updatedTodo.id);
-    if (!foundTodo) {
-      throw new Error('todo not found');
-    }
-    Object.assign(foundTodo, updatedTodo);
+  // // TODO replace with a get request
+  // // todos: Promise<Todo[]> = Promise.resolve(this.todoList);
 
-    return foundTodo;
+  // async addTodo(title: string): Promise<Todo> {
+  //   // TODO: replace with a POST request
+  //   const todo = {
+  //     id: this.todoId++,
+  //     title: title,
+  //     completed: false,
+  //   };
+  //   this.todoList.push(todo);
+
+  //   return todo;
+  // }
+
+  // async updateTodo(updatedTodo: Todo): Promise<Todo> {
+  //   // TODO: replace with a PUT request
+  //   const foundTodo = this.todoList.find((todo) => todo.id === updatedTodo.id);
+  //   if (!foundTodo) {
+  //     throw new Error('todo not found');
+  //   }
+  //   Object.assign(foundTodo, updatedTodo);
+
+  //   return foundTodo;
+  // }
+
+  getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${environment.apiUrl}/PiotrSadolewski/todo`);
   }
 }
+
