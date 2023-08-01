a66238647678e92d76c50836cda543262accbd35
Michał Klukowski <michal.klukowski@talan.com>
2023-08-01 12:26:00 +0200

    Extract todo items to separate component

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 
 
 
 @NgModule({
   declarations: [
-    TodoListComponent
+    TodoListComponent,
+    TodoItemComponent
   ],
   imports: [
     CommonModule,
