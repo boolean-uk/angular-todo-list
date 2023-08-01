9187ba9603b4e3861ffdd5e2711f53438b061c8f
Michał Klukowski <michal.klukowski@talan.com>
2023-08-01 14:58:09 +0200

    Unsubscribe on component destruction

--- src/app/todo/todo-item/todo-item.component.ts
+++ src/app/todo/todo-item/todo-item.component.ts
@@ -1,24 +1,31 @@
-import { Component, EventEmitter, HostListener, Input, Output } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, OnDestroy, Output } from '@angular/core';
 import { Todo } from '../model';
 import { TodoService } from '../services/todo.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
   styleUrls: ['./todo-item.component.css']
 })
-export class TodoItemComponent {
+export class TodoItemComponent implements OnDestroy {
   @Input() todo!: Todo
   @Output() onDelete = new EventEmitter<Todo>()
+
+  private toggleSub?: Subscription
 
   constructor(private todoService: TodoService) {}
 
   @HostListener("click") toggleCompleted() {
-    this.todoService.toggleTodo(this.todo)
+    this.toggleSub = this.todoService.toggleTodo(this.todo)
       .subscribe(newTodo => this.todo.completed = newTodo.completed)
   }
 
   delete() {
     this.onDelete.emit(this.todo)
+  }
+
+  ngOnDestroy(): void {
+    this.toggleSub?.unsubscribe()
   }
 }
