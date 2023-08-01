d32c49ed69339f083c153d5eabe5e090a757fd7e
Jakub Łaba <77542257+jakublaba@users.noreply.github.com>
2023-08-01 15:57:46 +0000

    Implement basic todo list operations (#1)
    * Implement TodoService
    
    * Update dependencies
    
    * Implement (most of) core
    
    * Make todo list more responsive
    
    * Implement filtering by completion status

--- src/app/todo/services/todo.service.ts
+++ src/app/todo/services/todo.service.ts
@@ -1,21 +1,51 @@
-import {configDotenv} from "dotenv";
-import {Injectable, OnInit} from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Todo} from "@app/todo/models/todo";
-import {Observable, of} from "rxjs";
+import {BehaviorSubject, catchError, ignoreElements, map, Observable, of, Subject, switchMap, tap} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {environment as env} from "@env/environment";
 
 @Injectable({
   providedIn: 'root',
 })
-export class TodoService implements OnInit {
-  addTodo(title: string): Observable<Todo | undefined> {
-    return of(undefined);
+export class TodoService {
+  private refresh$ = new BehaviorSubject<void>(undefined);
+  private update$ = new Subject<Todo>();
+  private updateErrs$: Observable<void | Error> = this.update$
+    .pipe(switchMap((todo) => this.http.put<Todo>(this.url(todo.id), todo).pipe(
+      ignoreElements(), catchError((err) => of(err))
+    )));
+  private todos$ = this.refresh$.pipe(switchMap(() => this.http.get<Todo[]>(this.url())));
+
+  constructor(private readonly http: HttpClient) {
   }
 
-  updateTodo(updatedTodo: Todo): Observable<Todo | undefined> {
-    return of(undefined);
+  get errs() {
+    return this.updateErrs$;
   }
 
-  ngOnInit(): void {
-    configDotenv();
+  add(title: string): Observable<Todo> {
+    return this.http.post<Todo>(this.url(), {title}).pipe(tap(() => this.refresh$.next()));
+  }
+
+  getOne(id: number): Observable<Todo> {
+    return this.http.get<Todo>(this.url(id));
+  }
+
+  getAll(completedFilter: boolean): Observable<Todo[]> {
+    return this.todos$.pipe(
+      map(todos => todos.filter(t => t.completed === completedFilter))
+    );
+  }
+
+  update(id: number, updatedTodo: Todo) {
+    this.update$.next(updatedTodo);
+  }
+
+  delete(id: number): Observable<Todo> {
+    return this.http.delete<Todo>(this.url(id));
+  }
+
+  private url(id?: number): string {
+    return `${env.apiUrl}/${env.username}/todo${id ? `/${id}` : ''}`;
   }
 }
--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,23 +1,35 @@
-import { Component } from '@angular/core';
-import { TodoService } from '../services/todo.service';
-import { Todo } from '../models/todo';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {TodoService} from "@app/todo/services/todo.service";
+import {Todo} from "@app/todo/models/todo";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css'],
 })
-export class TodoListComponent {
-  constructor(private readonly todoService: TodoService) {}
+export class TodoListComponent implements OnInit, OnDestroy {
+  subs: Subscription[] = [];
+  todos: Todo[] = [];
+  updateErrs$ = this.todoService.errs;
+  completedFilter = false;
 
-  todos = this.todoService.todos;
-
-  updateTodo(todo: Todo) {
-    this.todoService.updateTodo(todo);
+  constructor(private readonly todoService: TodoService) {
   }
 
-  async newTodo(title: string) {
-    await this.todoService.addTodo(title);
-    this.todos = this.todoService.todos;
+  toggleTodoFilter() {
+    this.completedFilter = !this.completedFilter;
+  }
+
+  ngOnInit(): void {
+    const sub = this.todoService.getAll(this.completedFilter)
+      .subscribe(todos => {
+        this.todos = todos;
+      });
+    this.subs.push(sub);
+  }
+
+  ngOnDestroy() {
+    this.subs.forEach(s => s.unsubscribe());
   }
 }
