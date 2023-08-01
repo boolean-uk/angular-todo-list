65e98b02146bc97651df4b66081f9fc2a2aecde8
Anna Sperkach <annsperkach@gmail.com>
2023-08-01 17:27:16 +0300

    Extension 2 and 3: adding button for completed/uncompleted todo list and show non-completed by default

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../services/todo.service';
 import { Todo } from '../models/todo';
-import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-todo-list',
@@ -9,31 +8,41 @@ import { Observable, of } from 'rxjs';
   styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent implements OnInit {
-  todos$: Observable<Todo[]> = of([]);
-
   constructor(private readonly todoService: TodoService) {}
+  completed: boolean = false;
+  todos: Todo[] = [];
 
-  ngOnInit(): void {
-    this.loadTodos(); 
+  async ngOnInit() {
+    this.todos = await this.todoService.getCompletedTodos(); 
   }
 
-  loadTodos() {
-    this.todos$ = this.todoService.getTodos();
+  async switch(){
+    this.completed = !this.completed;
+    if(!this.completed){
+      this.todos = await this.todoService.getCompletedTodos();
+    }
   }
-
+  
   async newTodo(title: string) {
     try {
-      await this.todoService.addTodo(title).toPromise();
-      this.loadTodos(); 
+      await this.todoService.addTodo(title);
+      this.todos = await this.todoService.getCompletedTodos();
     } catch (error) {
       console.error('Error adding todo:', error);
     }
   }
 
-  updateTodo(todo: Todo) {
-    this.todoService.updateTodo(todo).subscribe(
-      () => console.log('Todo updated successfully'),
-      (error) => console.error('Error updating todo:', error)
-    );
+  async updateTodo(todo: Todo) {
+    await this.todoService.updateTodo(todo);
+    if(!this.completed) {
+      this.todos = await this.todoService.getCompletedTodos();
+    }
+  }
+
+  async deleteTodo(todo: Todo) {
+    await this.todoService.deleteTodo(todo.id);
+    if(!this.completed) {
+      this.todos = await this.todoService.getCompletedTodos();
+    }
   }
 }
