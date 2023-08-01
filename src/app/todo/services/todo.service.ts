5160c97fc2f09e01bc705531fccb00b591bc57af
Jakub Łaba <77542257+jakublaba@users.noreply.github.com>
2023-08-01 12:56:32 +0200

    Remove template code

--- src/app/todo/services/todo.service.ts
+++ src/app/todo/services/todo.service.ts
@@ -1,52 +1,21 @@
-import { Injectable } from '@angular/core';
-import { Todo } from '../models/todo';
+import {configDotenv} from "dotenv";
+import {Injectable, OnInit} from '@angular/core';
+import {Todo} from "@app/todo/models/todo";
+import {Observable, of} from "rxjs";
 
 @Injectable({
   providedIn: 'root',
 })
-export class TodoService {
-  private todoId = 1;
-  private todoList: Todo[] = [
-    {
-      id: this.todoId++,
-      title: 'serve the app',
-      completed: true,
-    },
-    {
-      id: this.todoId++,
-      title: 'familiarise yourself with the codebase',
-      completed: false,
-    },
-    {
-      id: this.todoId++,
-      title: 'start talking to the api',
-      completed: false,
-    },
-  ];
-
-  // TODO replace with a get request
-  todos: Promise<Todo[]> = Promise.resolve(this.todoList);
-
-  async addTodo(title: string): Promise<Todo> {
-    // TODO: replace with a POST request
-    const todo = {
-      id: this.todoId++,
-      title: title,
-      completed: false,
-    };
-    this.todoList.push(todo);
-
-    return todo;
+export class TodoService implements OnInit {
+  addTodo(title: string): Observable<Todo | undefined> {
+    return of(undefined);
   }
 
-  async updateTodo(updatedTodo: Todo): Promise<Todo> {
-    // TODO: replace with a PUT request
-    const foundTodo = this.todoList.find((todo) => todo.id === updatedTodo.id);
-    if (!foundTodo) {
-      throw new Error('todo not found');
-    }
-    Object.assign(foundTodo, updatedTodo);
+  updateTodo(updatedTodo: Todo): Observable<Todo | undefined> {
+    return of(undefined);
+  }
 
-    return foundTodo;
+  ngOnInit(): void {
+    configDotenv();
   }
 }
