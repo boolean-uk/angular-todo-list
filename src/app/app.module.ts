968048354c9a0c57c663325289f316ef2caf04b4
Lewis <ljoncb@gmail.com>
2023-08-01 08:19:03 +0100

    added reference to freedom branch in README

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TodoModule } from './todo/todo.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, TodoModule],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
