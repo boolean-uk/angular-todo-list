34c40d8f16ff52734626990c5de5148541ccda8d
Anastasiya <ronas0709@gmail.com>
2023-08-01 14:16:38 +0200

    solve task + 1 extension

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,23 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../services/todo.service';
 import { Todo } from '../models/todo';
+
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css'],
-})
-export class TodoListComponent {
+}) 
+export class TodoListComponent implements OnInit {
   constructor(private readonly todoService: TodoService) {}
+  todos: Todo[] | null = null;
+  async ngOnInit() {
+    this.todos = await this.todoService.getAllTodos();
+    console.log(this.todos);
+}
 
-  todos = this.todoService.todos;
-
-  updateTodo(todo: Todo) {
+  async updateTodo(todo: Todo) {
     this.todoService.updateTodo(todo);
   }
 
   async newTodo(title: string) {
     await this.todoService.addTodo(title);
-    this.todos = this.todoService.todos;
+    this.todos = await this.todoService.getAllTodos();
+  }
+  async deleteTodo(todo:Todo) {
+    await this.todoService.deleteTodo(todo);
+    this.todos = await this.todoService.getAllTodos();
   }
 }
