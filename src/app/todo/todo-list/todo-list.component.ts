b36738c0ffbead2157b02e55d3acdba377817edc
Kamil Kulak <kamil.kulak@talan.com>
2023-08-01 16:47:41 +0200

    rebase failure :)

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -9,16 +9,9 @@ import { Todo } from '../models/todo';
 })
 export class TodoListComponent {
   constructor(private readonly todoService: TodoService) {}
-<<<<<<< HEAD
-  @Output('showCompleted') showCompleted = new EventEmitter();
-
-  todos = this.todoService.todos;
-  shouldShowCompleted = false
-=======
 
   todos = this.todoService.todos;
   ifShouldShowCompleted = false
->>>>>>> 9b2ed49 (Task finished with extensions)
 
   updateTodo(todo: Todo) {
     this.todoService.updateTodo(todo);
@@ -34,13 +27,8 @@ export class TodoListComponent {
     this.todos = this.todoService.todos;
   }
 
-<<<<<<< HEAD
-  printCompleted() {
-    this.shouldShowCompleted != this.shouldShowCompleted
-=======
   changeShowingCompleted() {
     this.ifShouldShowCompleted = !this.ifShouldShowCompleted
     console.log("change")
->>>>>>> 9b2ed49 (Task finished with extensions)
   }
 }
