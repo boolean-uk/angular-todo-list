512ccc2fd2ba49415c6e621307ff83c562c29e59
Justyna Dudar <justyna.dudar@talan.com>
2023-08-01 13:23:26 +0200

    delete works

--- src/app/todo/services/todo.service.ts
+++ src/app/todo/services/todo.service.ts
@@ -1,50 +1,57 @@
-import {Injectable} from '@angular/core';
-import {Todo} from '../models/todo';
-import {environment} from "../../../environments/environment";
-import {Observable} from "rxjs";
-import {HttpClient} from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { Todo } from '../models/todo';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
-
   // TODO replace with a get request
-  todos: Observable<Todo[]> | null = null
+  todos: Observable<Todo[]> | null = null;
   private todoId: number = 1;
-  private readonly API: string = environment.apiUrl
-  private readonly API_GET_TODOS: string = `${this.API}/${environment.user}/todo`
-  private readonly API_POST_TODO: string = `${this.API}/${environment.user}/todo`
-  private readonly API_PUT_TODO: string = `${this.API}/${environment.user}/todo/`
+  private readonly API: string = environment.apiUrl;
+  private readonly API_GET_TODOS: string = `${this.API}/${environment.user}/todo`;
+  private readonly API_POST_TODO: string = `${this.API}/${environment.user}/todo`;
+  private readonly API_PUT_DELETE_TODO: string = `${this.API}/${environment.user}/todo/`;
 
   constructor(private readonly http: HttpClient) {
-    this.todos = this.getTodos()
+    this.todos = this.getTodos();
   }
 
   getTodos(): Observable<Todo[]> {
-    return this.http.get<Todo[]>(this.API_GET_TODOS)
+    return this.http.get<Todo[]>(this.API_GET_TODOS);
   }
 
   addTodo(title: string) {
     // TODO: replace with a POST request
     const todo = {
-      id: this.todoId++, title: title, completed: false,
+      id: this.todoId++,
+      title: title,
+      completed: false,
     };
 
-    return this.http.post(this.API_POST_TODO, todo)
+    return this.http.post(this.API_POST_TODO, todo);
   }
 
   updateTodo(updatedTodo: Todo) {
     // TODO: replace with a PUT request
-    let foundTodo
+    let foundTodo;
     this.todos?.subscribe((todoList: Todo[]) => {
       foundTodo = todoList.find((todo: Todo) => todo.id === updatedTodo.id);
       if (!foundTodo) {
         throw new Error('todo not found');
       }
-      this.http.put(this.API_PUT_TODO + foundTodo.id, updatedTodo).subscribe()
+      this.http
+        .put(this.API_PUT_DELETE_TODO + foundTodo.id, updatedTodo)
+        .subscribe();
 
       return foundTodo;
-    })
+    });
+  }
+
+  deleteTodo(updatedTodo: Todo) {
+    return this.http.delete(this.API_PUT_DELETE_TODO + updatedTodo.id);
   }
 }
--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,17 +1,17 @@
-import {Component, OnInit} from '@angular/core';
-import {TodoService} from '../services/todo.service';
-import {Todo} from '../models/todo';
-import {Observable} from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../services/todo.service';
+import { Todo } from '../models/todo';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-todo-list', templateUrl: './todo-list.component.html', styleUrls: ['./todo-list.component.css'],
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent implements OnInit {
   todos: Observable<Todo[]> | null = this.todoService.todos;
 
-  constructor(private readonly todoService: TodoService) {
-
-  }
+  constructor(private readonly todoService: TodoService) {}
 
   updateTodo(todo: Todo) {
     this.todoService.updateTodo(todo);
@@ -19,11 +19,23 @@ export class TodoListComponent implements OnInit {
 
   newTodo(title: string) {
     this.todoService.addTodo(title).subscribe(() => {
-      this.todos = this.todoService.getTodos()
+      this.todos = this.todoService.getTodos();
     });
   }
 
   ngOnInit() {
-    this.todos = this.todoService.getTodos()
+    this.todos = this.todoService.getTodos();
+  }
+
+  deleteTodo(todo: Todo) {
+    this.todoService.deleteTodo(todo).subscribe({
+      next: (value) => {
+        this.todos = this.todoService.getTodos();
+      },
+      error: (error) => {
+        throw new Error('todo not found');
+      },
+      complete: () => {},
+    });
   }
 }
