a4c09d5cb1842ab9f1a4c0190a3d9ad80f47f86b
Alexander <alexander.ellnestam@gmail.com>
2024-03-12 09:47:46 +0100

    Done

--- src/app/todo/services/todo.service.ts
+++ src/app/todo/services/todo.service.ts
@@ -1,52 +1,39 @@
 import { Injectable } from '@angular/core';
 import { Todo } from '../models/todo';
+import { Observable, firstValueFrom, map } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
   private todoId = 1;
-  private todoList: Todo[] = [
-    {
-      id: this.todoId++,
-      title: 'serve the app',
-      completed: true,
-    },
-    {
-      id: this.todoId++,
-      title: 'familiarise yourself with the codebase',
-      completed: false,
-    },
-    {
-      id: this.todoId++,
-      title: 'start talking to the api',
-      completed: false,
-    },
-  ];
+  
+  constructor(private readonly http: HttpClient) {
 
-  // TODO replace with a get request
-  todos: Promise<Todo[]> = Promise.resolve(this.todoList);
+  }
+  
 
-  async addTodo(title: string): Promise<Todo> {
-    // TODO: replace with a POST request
-    const todo = {
-      id: this.todoId++,
+  addTodo(title: string): Observable<Todo> {
+
+    const toCreate = {
       title: title,
-      completed: false,
     };
-    this.todoList.push(todo);
-
-    return todo;
+    return this.http.post<Todo>(`${environment.apiUrl}/Alexanderell/todo`, toCreate)
   }
 
-  async updateTodo(updatedTodo: Todo): Promise<Todo> {
-    // TODO: replace with a PUT request
-    const foundTodo = this.todoList.find((todo) => todo.id === updatedTodo.id);
-    if (!foundTodo) {
-      throw new Error('todo not found');
-    }
-    Object.assign(foundTodo, updatedTodo);
+  getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${environment.apiUrl}/Alexanderell/todo`);
+    
+  }
 
-    return foundTodo;
+  updateTodo(todo: Todo): Observable<Todo> {
+    return this.http.put<Todo>(`${environment.apiUrl}/Alexanderell/todo/${todo.id}`, todo)
+  }
+
+  deleteTodoById(id: number): Observable<void> {
+    return this.http.delete<void>(`${environment.apiUrl}/Alexanderell/todo/${id}`);
   }
 }
