b1d8ba5eb6acc0e1752383bcc0f5595a5491dd23
Linus Willmont <linus.willmont@hotmail.com>
2024-03-13 11:42:49 +0100

    Update todo

--- src/app/todo/services/todo.service.ts
+++ src/app/todo/services/todo.service.ts
@@ -7,39 +7,29 @@ import { firstValueFrom } from 'rxjs';
   providedIn: 'root',
 })
 export class TodoService {
+  BASE_URL = 'https://boolean-api-server.fly.dev/LinusWillmont/todo';
   todos: Promise<Todo[]> = this.getTodos();
 
   constructor(private http: HttpClient) {}
 
-  // TODO replace with a get request
   async getTodos(): Promise<Todo[]> {
-    return await firstValueFrom(
-      this.http.get<Todo[]>(
-        'https://boolean-api-server.fly.dev/LinusWillmont/todo'
-      )
-    );
+    return await firstValueFrom(this.http.get<Todo[]>(this.BASE_URL));
   }
 
   async addTodo(title: string): Promise<Todo> {
     const todo = await firstValueFrom(
-      this.http.post<Todo>(
-        'https://boolean-api-server.fly.dev/LinusWillmont/todo',
-        { title: title }
-      )
+      this.http.post<Todo>(this.BASE_URL, { title: title })
     );
     (await this.todos).push(todo);
     return todo;
-    //TODO: add the newly created todo to the todolist to show it
   }
 
-  // async updateTodo(updatedTodo: Todo): Promise<Todo> {
   async updateTodo(updatedTodo: Todo) {
-    // // TODO: replace with a PUT request
-    // const foundTodo = this.todos.find((todo) => todo.id === updatedTodo.id);
-    // if (!foundTodo) {
-    //   throw new Error('todo not found');
-    // }
-    // Object.assign(foundTodo, updatedTodo);
-    // return foundTodo;
+    await firstValueFrom(
+      this.http.put(`${this.BASE_URL}/${updatedTodo.id}`, {
+        title: updatedTodo.title,
+        completed: updatedTodo.completed,
+      })
+    );
   }
 }
