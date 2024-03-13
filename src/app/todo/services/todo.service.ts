2b562584f88a99a51acf9d367c71d8eeddd9e31f
noahlenn <noahlenn@hotmail.com>
2024-03-13 14:31:50 +0100

    extension 2

--- src/app/todo/services/todo.service.ts
+++ src/app/todo/services/todo.service.ts
@@ -8,39 +8,10 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root',
 })
 export class TodoService {
-  private todoId = 1;
-  private todoList: Todo[] = [
-    {
-      id: this.todoId++,
-      title: 'serve the app',
-      completed: true,
-    },
-    {
-      id: this.todoId++,
-      title: 'familiarise yourself with the codebase',
-      completed: false,
-    },
-    {
-      id: this.todoId++,
-      title: 'start talking to the api',
-      completed: false,
-    },
-  ];
 
   http = inject(HttpClient)
-  // todos: any;
 
 
-  // TODO replace with a get request
-  // todos: Promise<Todo[]> = Promise.resolve(this.todoList);
-  // First try:
-    // async getTodos() {
-    //   const result = await firstValueFrom(this.http.get(`${environment.apiUrl}/todo`));
-    //   // @ts-ignore
-    //   this.todos = result.results;
-    //   return this.todos
-    // }
-  // Second try:
   get todos(): Promise<Todo[]> {
     // @ts-ignore
     return firstValueFrom(this.http.get(`${environment.apiUrl}/noahlenn/todo`));
@@ -48,36 +19,17 @@ export class TodoService {
 
 
   async addTodo(title: string): Promise<Todo> {
-    // TODO: replace with a POST request
-    // const todo = {
-    //   id: this.todoId++,
-    //   title: title,
-    //   completed: false,
-    // };
-    // this.todoList.push(todo);
-
-    // return todo;
     const todo = await firstValueFrom(this.http.post(`${environment.apiUrl}/noahlenn/todo`, {title: title}))
     // @ts-ignore
 
     return todo
   }
 
-  async updateTodo(id: number): Promise<Todo> {
-    // TODO: replace with a PUT request
-    // const foundTodo = this.todoList.find((todo) => todo.id === updatedTodo.id);
-    // if (!foundTodo) {
-    //   throw new Error('todo not found');
-    // }
-    // Object.assign(foundTodo, updatedTodo);
-
-    // return foundTodo;
-    
-    
+  async updateTodo(id: number): Promise<Todo> {  
     const todo = await firstValueFrom(this.http.get<Todo>(`${environment.apiUrl}/noahlenn/todo/${id}`));
     const updatedTodo = { ...todo, completed: !todo.completed };
     const updatedTodoResponse = await firstValueFrom(this.http.put<Todo>(`${environment.apiUrl}/noahlenn/todo/${id}`, updatedTodo));
-    
+
     return updatedTodoResponse;
 
   } 
