01d3eb84b620357bea2fc1343cedcc7a0f4d9ec5
Jens Arvid Forgård <jensarvid99@gmail.com>
2024-03-13 11:44:58 +0100

    Core + Ext

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -8,16 +8,29 @@ import { Todo } from '../models/todo';
   styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent {
-  constructor(private readonly todoService: TodoService) {}
+  
 
   todos = this.todoService.todos;
+  showDone: boolean = false
+  filteredTodos: Array<any> = []
 
+  constructor(private readonly todoService: TodoService) {
+    this.todos.then(todo => this.filteredTodos = todo.filter((task => !task.completed)))
+  }
   updateTodo(todo: Todo) {
-    this.todoService.updateTodo(todo);
+    this.todoService.updateTodo(todo).then((task) => {
+      if (task.completed !== this.showDone) this.filteredTodos.splice(this.filteredTodos.indexOf(task), 1)
+    })
   }
 
   async newTodo(title: string) {
     await this.todoService.addTodo(title);
     this.todos = this.todoService.todos;
+  }
+
+  toggleComplete(): void
+  {
+    this.showDone = !this.showDone
+    this.todos.then(todo => this.filteredTodos = todo.filter((task => task.completed === this.showDone)))
   }
 }
