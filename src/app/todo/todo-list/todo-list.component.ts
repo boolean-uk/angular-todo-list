5983b8107072c1a0d907b12d054cf16fbc677225
llllllll-l <vincent.werelius@gmail.com>
2024-03-13 11:11:40 +0100

    Core implemented and tested | done

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,23 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../services/todo.service';
 import { Todo } from '../models/todo';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css'],
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+  todos: Todo[] = [];
   constructor(private readonly todoService: TodoService) {}
 
-  todos = this.todoService.todos;
+  // todos = this.todoService.todos;
 
+  ngOnInit(): void {
+    this.loadTodos();
+  }
+
+  async loadTodos() {
+    //@ts-ignore
+    this.todos = await this.todoService.loadTodos();
+  }
   updateTodo(todo: Todo) {
-    this.todoService.updateTodo(todo);
+    console.log(todo);
+    this.todoService.updateTodo(todo).then(() => this.loadTodos());
   }
 
   async newTodo(title: string) {
     await this.todoService.addTodo(title);
-    this.todos = this.todoService.todos;
+    this.loadTodos();
   }
 }
