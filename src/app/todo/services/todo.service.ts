7227332474e3dcc474e5f5c54172bb1dd45921b5
henrikrosenkilde <henrik.rosenkilde@hotmail.com>
2024-03-13 12:01:02 +0100

    core+ext

--- src/app/todo/services/todo.service.ts
+++ src/app/todo/services/todo.service.ts
@@ -1,52 +1,45 @@
-import { Injectable } from '@angular/core';
+import { ApplicationRef, Injectable, inject } from '@angular/core';
 import { Todo } from '../models/todo';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
-  private todoId = 1;
-  private todoList: Todo[] = [
-    {
-      id: this.todoId++,
-      title: 'serve the app',
-      completed: true,
-    },
-    {
-      id: this.todoId++,
-      title: 'familiarise yourself with the codebase',
-      completed: false,
-    },
-    {
-      id: this.todoId++,
-      title: 'start talking to the api',
-      completed: false,
-    },
-  ];
+  private URL = "https://boolean-api-server.fly.dev/henrikrosenkilde/todo"
+  todos: any;
+  http = inject(HttpClient)
 
-  // TODO replace with a get request
-  todos: Promise<Todo[]> = Promise.resolve(this.todoList);
+  constructor(private appRef: ApplicationRef) {
+    this.todos = this.getTodos();
+  }
 
-  async addTodo(title: string): Promise<Todo> {
-    // TODO: replace with a POST request
-    const todo = {
-      id: this.todoId++,
-      title: title,
-      completed: false,
-    };
-    this.todoList.push(todo);
+  async getTodos() {
+    const result = await firstValueFrom(this.http.get(`${this.URL}`));
+    // @ts-ignore
+    this.todos = result;
+    return this.todos;
+  }
 
-    return todo;
+  async addTodo(title: string) {
+    const request = this.http.post<Todo>(this.URL, { title: title });
+    const response = await firstValueFrom(request);
+    this.todos.push(response)
   }
 
   async updateTodo(updatedTodo: Todo): Promise<Todo> {
-    // TODO: replace with a PUT request
-    const foundTodo = this.todoList.find((todo) => todo.id === updatedTodo.id);
-    if (!foundTodo) {
+    const request = this.http.put<Todo>(this.URL + "/" + updatedTodo.id, 
+    { 
+      title: updatedTodo.title, 
+      completed: updatedTodo.completed 
+    });
+    const foundTodo = firstValueFrom(request)
+    if (!foundTodo) 
+    {
       throw new Error('todo not found');
     }
     Object.assign(foundTodo, updatedTodo);
-
     return foundTodo;
   }
 }
