9ae25bb9e56604356edeff66ee043dab4270b5b7
Knut S Rostad <knut.rostad@no.experis.com>
2024-03-13 13:28:34 +0100

    all done

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -11,9 +11,26 @@ export class TodoListComponent {
   constructor(private readonly todoService: TodoService) {}
 
   todos = this.todoService.todos;
+  displayedTodos: Todo[] = [];
+  filtered: boolean = false;
+
+  async ngOnInit() {
+    this.displayedTodos = (await this.todos).filter((todo) => !todo.completed);
+  }
 
   updateTodo(todo: Todo) {
     this.todoService.updateTodo(todo);
+  }
+
+  async toggleFilter() {
+    //If toggled, show completed
+    if (this.filtered) {
+      this.displayedTodos = (await this.todos).filter(
+        (todo) => !todo.completed
+      );
+    } else {
+      this.displayedTodos = (await this.todos).filter((todo) => todo.completed);
+    }
   }
 
   async newTodo(title: string) {
