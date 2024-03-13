f9203291adf1ef3b06e3d0f6680559e26f9b9dd9
Elias <Elias@soprani.net>
2024-03-13 13:17:32 +0100

    core and extensions done

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../services/todo.service';
 import { Todo } from '../models/todo';
 
@@ -7,17 +7,32 @@ import { Todo } from '../models/todo';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css'],
 })
-export class TodoListComponent {
-  constructor(private readonly todoService: TodoService) {}
+export class TodoListComponent implements OnInit {
+  constructor(private readonly todoService: TodoService) { }
 
-  todos = this.todoService.todos;
+  todos: Todo[] = [];
+  filteredTodos: Todo[] = [];
+
+  async ngOnInit() {
+    this.todos = await this.todoService.todos;
+    this.showNotCompleted();
+  }
+
+  showCompleted() {
+    this.filteredTodos = this.todos.filter((todo) => todo.completed);
+  }
+
+  showNotCompleted() {
+    this.filteredTodos = this.todos.filter((todo) => !todo.completed);
+  }
 
   updateTodo(todo: Todo) {
     this.todoService.updateTodo(todo);
   }
 
   async newTodo(title: string) {
-    await this.todoService.addTodo(title);
-    this.todos = this.todoService.todos;
+    await this.todoService.postTodo(title);
+    this.todos = await this.todoService.todos;
+    this.showNotCompleted(); 
   }
 }
