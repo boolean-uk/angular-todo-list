b81f6555db3cacf6133945e85e13984c46bc12be
Marcus Palm <marcus.palm@se.experis.com>
2024-03-13 12:51:37 +0100

    Marcus Palm Core Done

--- src/app/todo/services/todo.service.ts
+++ src/app/todo/services/todo.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Todo } from '../models/todo';
-
+import { firstValueFrom } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+//@ts-ignore
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
-  private todoId = 1;
+  /* private todoId = 1;
   private todoList: Todo[] = [
     {
       id: this.todoId++,
@@ -22,31 +25,59 @@ export class TodoService {
       title: 'start talking to the api',
       completed: false,
     },
-  ];
+  ]; */
+  todos: Todo[] = [];
+  constructor(private readonly http: HttpClient) {
+    this.loadTodos();
+  }
 
   // TODO replace with a get request
-  todos: Promise<Todo[]> = Promise.resolve(this.todoList);
+  /* todos: Promise<Todo[]> = Promise.resolve(this.todoList); */
 
-  async addTodo(title: string): Promise<Todo> {
+  async loadTodos() {
+    this.todos = await firstValueFrom(
+      this.http.get<Todo[]>(`${environment.apiUrl}/MackanPalm/todo`)
+    );
+  }
+
+  async addTodo(title: string) {
     // TODO: replace with a POST request
-    const todo = {
+    /* const todo = {
       id: this.todoId++,
       title: title,
       completed: false,
     };
     this.todoList.push(todo);
 
-    return todo;
+    return todo; */
+
+    const todoToCreate = { title };
+    const todo = await firstValueFrom(
+      this.http.post<Todo>(
+        `${environment.apiUrl}/MackanPalm/todo`,
+        todoToCreate
+      )
+    );
+    this.todos = [...this.todos, todo];
+    await this.loadTodos();
   }
 
-  async updateTodo(updatedTodo: Todo): Promise<Todo> {
+  async updateTodo(updatedTodo: Todo) {
     // TODO: replace with a PUT request
-    const foundTodo = this.todoList.find((todo) => todo.id === updatedTodo.id);
+    /* const foundTodo = this.todoList.find((todo) => todo.id === updatedTodo.id);
     if (!foundTodo) {
       throw new Error('todo not found');
     }
-    Object.assign(foundTodo, updatedTodo);
+    Object.assign(foundTodo, updatedTodo); */
 
-    return foundTodo;
+    const todo = await firstValueFrom(
+      this.http.put<Todo>(
+        `${environment.apiUrl}/MackanPalm/todo/${updatedTodo.id}`,
+        updatedTodo
+      )
+    );
+
+    this.todos = this.todos.map((t) => (t.id === todo.id ? todo : t));
+    await this.loadTodos();
   }
 }
--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { TodoService } from '../services/todo.service';
 import { Todo } from '../models/todo';
-
+//@ts-ignore
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent {
-  constructor(private readonly todoService: TodoService) {}
+  constructor(public readonly todoService: TodoService) {}
 
   todos = this.todoService.todos;
 
