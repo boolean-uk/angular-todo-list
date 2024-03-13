3f552a36ec10e413f09a8289f37609992fe939cf
Victor <victor.adamson@se.experis.com>
2024-03-13 10:15:03 +0100

    Core done

--- src/app/todo/services/todo.service.ts
+++ src/app/todo/services/todo.service.ts
@@ -1,47 +1,44 @@
 import { Injectable } from '@angular/core';
 import { Todo } from '../models/todo';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
   private todoId = 1;
-  private todoList: Todo[] = [
-    {
-      id: this.todoId++,
-      title: 'serve the app',
-      completed: true,
-    },
-    {
-      id: this.todoId++,
-      title: 'familiarise yourself with the codebase',
-      completed: false,
-    },
-    {
-      id: this.todoId++,
-      title: 'start talking to the api',
-      completed: false,
-    },
-  ];
+  private todoList: Todo[] = [];
+  constructor(private readonly http: HttpClient) {}
 
-  // TODO replace with a get request
-  todos: Promise<Todo[]> = Promise.resolve(this.todoList);
-
+  get todos(): Promise<Todo[]> {
+    return firstValueFrom(
+      this.http.get<Todo[]>(
+        'https://boolean-api-server.fly.dev/VictorAdamson/todo'
+      )
+    );
+  }
   async addTodo(title: string): Promise<Todo> {
-    // TODO: replace with a POST request
-    const todo = {
-      id: this.todoId++,
-      title: title,
-      completed: false,
-    };
+    const newTask = { title };
+    const todo = await firstValueFrom(
+      this.http.post<Todo>(
+        `https://boolean-api-server.fly.dev/VictorAdamson/todo`,
+        newTask
+      )
+    );
     this.todoList.push(todo);
 
     return todo;
   }
 
   async updateTodo(updatedTodo: Todo): Promise<Todo> {
-    // TODO: replace with a PUT request
-    const foundTodo = this.todoList.find((todo) => todo.id === updatedTodo.id);
+    const foundTodo = await firstValueFrom(
+      this.http.put<Todo>(
+        `https://boolean-api-server.fly.dev/VictorAdamson/todo/${updatedTodo.id}`,
+        updatedTodo
+      )
+    );
+    console.log(foundTodo);
     if (!foundTodo) {
       throw new Error('todo not found');
     }
