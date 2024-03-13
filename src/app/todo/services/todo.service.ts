7ccdcb34712b921f2b2fed025464cfe244f409d0
Linus Willmont <linus.willmont@hotmail.com>
2024-03-13 11:30:56 +0100

    Get and Post todos

--- src/app/todo/services/todo.service.ts
+++ src/app/todo/services/todo.service.ts
@@ -1,52 +1,45 @@
 import { Injectable } from '@angular/core';
 import { Todo } from '../models/todo';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
-  private todoId = 1;
-  private todoList: Todo[] = [
-    {
-      id: this.todoId++,
-      title: 'serve the app',
-      completed: true,
-    },
-    {
-      id: this.todoId++,
-      title: 'familiarise yourself with the codebase',
-      completed: false,
-    },
-    {
-      id: this.todoId++,
-      title: 'start talking to the api',
-      completed: false,
-    },
-  ];
+  todos: Promise<Todo[]> = this.getTodos();
+
+  constructor(private http: HttpClient) {}
 
   // TODO replace with a get request
-  todos: Promise<Todo[]> = Promise.resolve(this.todoList);
-
-  async addTodo(title: string): Promise<Todo> {
-    // TODO: replace with a POST request
-    const todo = {
-      id: this.todoId++,
-      title: title,
-      completed: false,
-    };
-    this.todoList.push(todo);
-
-    return todo;
+  async getTodos(): Promise<Todo[]> {
+    return await firstValueFrom(
+      this.http.get<Todo[]>(
+        'https://boolean-api-server.fly.dev/LinusWillmont/todo'
+      )
+    );
   }
 
-  async updateTodo(updatedTodo: Todo): Promise<Todo> {
-    // TODO: replace with a PUT request
-    const foundTodo = this.todoList.find((todo) => todo.id === updatedTodo.id);
-    if (!foundTodo) {
-      throw new Error('todo not found');
-    }
-    Object.assign(foundTodo, updatedTodo);
+  async addTodo(title: string): Promise<Todo> {
+    const todo = await firstValueFrom(
+      this.http.post<Todo>(
+        'https://boolean-api-server.fly.dev/LinusWillmont/todo',
+        { title: title }
+      )
+    );
+    (await this.todos).push(todo);
+    return todo;
+    //TODO: add the newly created todo to the todolist to show it
+  }
 
-    return foundTodo;
+  // async updateTodo(updatedTodo: Todo): Promise<Todo> {
+  async updateTodo(updatedTodo: Todo) {
+    // // TODO: replace with a PUT request
+    // const foundTodo = this.todos.find((todo) => todo.id === updatedTodo.id);
+    // if (!foundTodo) {
+    //   throw new Error('todo not found');
+    // }
+    // Object.assign(foundTodo, updatedTodo);
+    // return foundTodo;
   }
 }
