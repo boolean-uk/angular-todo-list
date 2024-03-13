eed33df3c38ce71a3f81c9a301a5e0aa8eaba096
Gudbrand <gudbrandwd@live.no>
2024-03-13 13:10:01 +0100

    core

--- src/app/todo/services/todo.service.ts
+++ src/app/todo/services/todo.service.ts
@@ -1,5 +1,8 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Todo } from '../models/todo';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -23,30 +26,44 @@ export class TodoService {
       completed: false,
     },
   ];
+  http = inject(HttpClient);
 
-  // TODO replace with a get request
-  todos: Promise<Todo[]> = Promise.resolve(this.todoList);
+  get todos(): Promise<Todo[]> {
+    // @ts-ignore
+    return firstValueFrom(this.http.get(`${environment.apiUrl}/todo`));
+  }
 
   async addTodo(title: string): Promise<Todo> {
-    // TODO: replace with a POST request
     const todo = {
-      id: this.todoId++,
+      id: 0,
       title: title,
       completed: false,
     };
-    this.todoList.push(todo);
-
-    return todo;
+    // @ts-ignore
+    const newTodo: Todo = await firstValueFrom(
+      this.http.post(`${environment.apiUrl}/todo`, todo)
+    );
+    this.todoList.push(newTodo);
+    return newTodo;
   }
 
-  async updateTodo(updatedTodo: Todo): Promise<Todo> {
-    // TODO: replace with a PUT request
+  async updateTodo(todoToUpdate: Todo): Promise<Todo> {
+    const newTodo = {
+      title: todoToUpdate.title,
+      completed: todoToUpdate.completed,
+    };
+    // @ts-ignore
+    const updatedTodo: Todo = await firstValueFrom(
+      this.http.put(`${environment.apiUrl}/todo/` + todoToUpdate.id, newTodo)
+    );
+    if (!updatedTodo) {
+      throw new Error('todo not found');
+    }
     const foundTodo = this.todoList.find((todo) => todo.id === updatedTodo.id);
     if (!foundTodo) {
       throw new Error('todo not found');
     }
     Object.assign(foundTodo, updatedTodo);
-
     return foundTodo;
   }
 }
