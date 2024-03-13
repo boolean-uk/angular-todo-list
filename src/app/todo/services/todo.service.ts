ed76773b19a5f4ff4c224f4233a2768135af6552
Espen Solhaug <espen.solhaug@no.experis.com>
2024-03-13 12:23:45 +0100

    core ✅

--- src/app/todo/services/todo.service.ts
+++ src/app/todo/services/todo.service.ts
@@ -1,52 +1,42 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Todo } from '../models/todo';
+import { environment } from 'src/environments/environment';
+import { firstValueFrom } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
-  private todoId = 1;
-  private todoList: Todo[] = [
-    {
-      id: this.todoId++,
-      title: 'serve the app',
-      completed: true,
-    },
-    {
-      id: this.todoId++,
-      title: 'familiarise yourself with the codebase',
-      completed: false,
-    },
-    {
-      id: this.todoId++,
-      title: 'start talking to the api',
-      completed: false,
-    },
-  ];
+  http = inject(HttpClient);
 
-  // TODO replace with a get request
-  todos: Promise<Todo[]> = Promise.resolve(this.todoList);
+  get todos(): Promise<Todo[]> {
+    // @ts-ignore
+    return this.http.get(`${environment.apiUrl}/espensolhaug1/todo`);
+  }
 
   async addTodo(title: string): Promise<Todo> {
     // TODO: replace with a POST request
-    const todo = {
-      id: this.todoId++,
-      title: title,
-      completed: false,
-    };
-    this.todoList.push(todo);
-
+    const todo = await firstValueFrom(
+      this.http.post(`${environment.apiUrl}/espensolhaug1/todo`, {
+        title: title,
+      })
+    );
+    // @ts-ignore
     return todo;
   }
 
   async updateTodo(updatedTodo: Todo): Promise<Todo> {
-    // TODO: replace with a PUT request
-    const foundTodo = this.todoList.find((todo) => todo.id === updatedTodo.id);
-    if (!foundTodo) {
-      throw new Error('todo not found');
-    }
-    Object.assign(foundTodo, updatedTodo);
-
-    return foundTodo;
+    const todo = await firstValueFrom(
+      this.http.put(
+        `${environment.apiUrl}/espensolhaug1/todo/${updatedTodo.id}`,
+        {
+          title: updatedTodo.title,
+          completed: updatedTodo.completed,
+        }
+      )
+    );
+    // @ts-ignore
+    return todo;
   }
 }
