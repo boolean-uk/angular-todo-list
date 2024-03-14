090227e9e389bae95548014970d2b5967a216f2b
guro18 <rollgustav95@gmail.com>
2024-03-14 15:11:25 +0100

    updated based on feedback

--- src/app/todo/services/todo.service.ts
+++ src/app/todo/services/todo.service.ts
@@ -7,35 +7,16 @@ import { firstValueFrom } from 'rxjs';
   providedIn: 'root',
 })
 export class TodoService {
-  // private todoId = 1;
-  // private todoList: Todo[] = [
-  //   {
-  //     id: this.todoId++,
-  //     title: 'serve the app',
-  //     completed: true,
-  //   },
-  //   {
-  //     id: this.todoId++,
-  //     title: 'familiarise yourself with the codebase',
-  //     completed: false,
-  //   },
-  //   {
-  //     id: this.todoId++,
-  //     title: 'start talking to the api',
-  //     completed: false,
-  //   },
-  // ];
 
   constructor(private readonly http: HttpClient) {}
 
-  // TODO replace with a get request
   get todos(): Promise<Todo[]> {
     return firstValueFrom(this.http.get<Todo[]>
       ('https://boolean-api-server.fly.dev/guro18/todo'));
   }
 
   async addTodo(title: string) {
-    // TODO: replace with a POST request
+
     const todo = {
       title: title,
     };
@@ -47,7 +28,7 @@ export class TodoService {
   }
 
   async updateTodo(updatedTodo: Todo) {
-    // TODO: replace with a PUT request
+
     const put = await firstValueFrom(
       this.http.put<Todo>(
         `https://boolean-api-server.fly.dev/guro18/todo/${updatedTodo.id}`,
