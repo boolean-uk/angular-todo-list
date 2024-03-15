e1ed154a236613c40b052b2a297e3007b9365d62
Kristian <70452674+Solido7@users.noreply.github.com>
2024-03-15 10:06:28 +0100

    core

--- src/app/todo/services/todo.service.ts
+++ src/app/todo/services/todo.service.ts
@@ -1,47 +1,34 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Todo } from '../models/todo';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
-  private todoId = 1;
-  private todoList: Todo[] = [
-    {
-      id: this.todoId++,
-      title: 'serve the app',
-      completed: true,
-    },
-    {
-      id: this.todoId++,
-      title: 'familiarise yourself with the codebase',
-      completed: false,
-    },
-    {
-      id: this.todoId++,
-      title: 'start talking to the api',
-      completed: false,
-    },
-  ];
+  http = inject(HttpClient);
+  todos: Promise<Todo[]> = this.getTodos();
 
-  // TODO replace with a get request
-  todos: Promise<Todo[]> = Promise.resolve(this.todoList);
+  async getTodos(): Promise<Array<Todo>> {
+    return await firstValueFrom(this.http.get(`https://boolean-api-server.fly.dev/Solido7/todo`)) as Array<Todo>;
+  }
 
   async addTodo(title: string): Promise<Todo> {
-    // TODO: replace with a POST request
     const todo = {
-      id: this.todoId++,
       title: title,
       completed: false,
     };
-    this.todoList.push(todo);
-
-    return todo;
+    
+    const result = await firstValueFrom(this.http.post(`https://boolean-api-server.fly.dev/Solido7/todo`, todo)) as Todo;
+    this.todos.then(async (todos) => {
+      todos.push(result);
+    });
+    return result;
   }
 
   async updateTodo(updatedTodo: Todo): Promise<Todo> {
-    // TODO: replace with a PUT request
-    const foundTodo = this.todoList.find((todo) => todo.id === updatedTodo.id);
+    const foundTodo = await firstValueFrom(this.http.put(`https://boolean-api-server.fly.dev/Solido7/todo/${updatedTodo.id}`, updatedTodo)) as Todo;
     if (!foundTodo) {
       throw new Error('todo not found');
     }
