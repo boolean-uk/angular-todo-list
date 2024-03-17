a3f664686dcedde79004da14519c37d3a4ea8768
Petros Mylonas <petrosmylonascs@gmail.com>
2024-03-17 14:37:28 +0200

    Petros Mylonas

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,23 +1,32 @@
 import { Component } from '@angular/core';
 import { TodoService } from '../services/todo.service';
 import { Todo } from '../models/todo';
-
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent {
+  todos$: Observable<Todo[]> | undefined;
   constructor(private readonly todoService: TodoService) {}
 
-  todos = this.todoService.todos;
+  ngOnInit(): void {
+    this.loadTodos();
+  }
+
+  loadTodos() {
+    this.todos$ = this.todoService.todos;
+  }
 
   updateTodo(todo: Todo) {
-    this.todoService.updateTodo(todo);
+    this.todoService.updateTodo(todo).subscribe(() => {
+      this.loadTodos();
+    });
   }
 
   async newTodo(title: string) {
-    await this.todoService.addTodo(title);
-    this.todos = this.todoService.todos;
+    await this.todoService.addTodo(title).toPromise();
+    this.loadTodos();
   }
 }
