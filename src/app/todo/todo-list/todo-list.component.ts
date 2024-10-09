dcf83cd03ee3b3c7d86a36dbe33f6f825e881213
JosteinRuen <99617155+JosteinRuen@users.noreply.github.com>
2024-10-09 14:31:49 +0200

    core + ext

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,23 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../services/todo.service';
 import { Todo } from '../models/todo';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css'],
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+  todos$: Observable<Todo[]> | null = null;
+
+  showCompleted: boolean = true;
+
   constructor(private readonly todoService: TodoService) {}
 
-  todos = this.todoService.todos;
-
-  updateTodo(todo: Todo) {
-    this.todoService.updateTodo(todo);
+  ngOnInit() {
+    this.todos$ = this.todoService.todos.pipe(
+      map(todos => todos.filter(todo => todo.completed === this.showCompleted))
+    );
   }
 
-  async newTodo(title: string) {
-    await this.todoService.addTodo(title);
-    this.todos = this.todoService.todos;
+  updateTodo(todo: Todo) {
+    this.todoService.updateTodo(todo).subscribe();
+  }
+
+  newTodo(title: string) {
+    this.todoService.addTodo(title).subscribe(() => {
+      this.todos$ = this.todoService.todos.pipe(
+        map(todos => todos.filter(todo => todo.completed === this.showCompleted))
+      );
+    });
+  }
+
+  toggleShowCompleted() {
+    this.showCompleted = !this.showCompleted;
+    this.todos$ = this.todoService.todos.pipe(
+      map(todos => todos.filter(todo => todo.completed === this.showCompleted))
+    );
   }
 }
--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoCreateComponent } from './todo-create/todo-create.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
-import { FormsModule } from '@angular/forms';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 
-@NgModule({ declarations: [TodoListComponent, TodoCreateComponent, TodoItemComponent],
-    exports: [TodoCreateComponent, TodoListComponent], imports: [CommonModule, FormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
+@NgModule({
+  declarations: [TodoListComponent, TodoCreateComponent, TodoItemComponent],
+  exports: [TodoCreateComponent, TodoListComponent],
+  imports: [CommonModule, FormsModule, HttpClientModule],
+})
 export class TodoModule {}
