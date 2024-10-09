82ba0a42344823eb833887c3a862db95ba63a4f7
Fredrik Haupt <fredrik.haupt@se.experis.com>
2024-10-09 15:33:13 +0200

    commit

--- src/app/todo/services/todo.service.ts
+++ src/app/todo/services/todo.service.ts
@@ -1,52 +1,25 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Todo } from '../models/todo';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
-  private todoId = 1;
-  private todoList: Todo[] = [
-    {
-      id: this.todoId++,
-      title: 'serve the app',
-      completed: true,
-    },
-    {
-      id: this.todoId++,
-      title: 'familiarise yourself with the codebase',
-      completed: false,
-    },
-    {
-      id: this.todoId++,
-      title: 'start talking to the api',
-      completed: false,
-    },
-  ];
+  private httpClient = inject(HttpClient)
 
-  // TODO replace with a get request
-  todos: Promise<Todo[]> = Promise.resolve(this.todoList);
-
-  async addTodo(title: string): Promise<Todo> {
-    // TODO: replace with a POST request
-    const todo = {
-      id: this.todoId++,
-      title: title,
-      completed: false,
-    };
-    this.todoList.push(todo);
-
-    return todo;
+  async todos(): Promise<Todo[]>{
+    return firstValueFrom(this.httpClient.get<Todo[]>("https://boolean-uk-api-server.fly.dev/FredrikEH/todo"))
   }
 
+  async addTodo(title: string): Promise<Todo> {
+    const todo = await firstValueFrom(this.httpClient.post<Todo>("https://boolean-uk-api-server.fly.dev/FredrikEH/todo", {title}))
+    window.location.reload()
+    return todo
+  }
+  
   async updateTodo(updatedTodo: Todo): Promise<Todo> {
-    // TODO: replace with a PUT request
-    const foundTodo = this.todoList.find((todo) => todo.id === updatedTodo.id);
-    if (!foundTodo) {
-      throw new Error('todo not found');
-    }
-    Object.assign(foundTodo, updatedTodo);
-
-    return foundTodo;
+    return await firstValueFrom(this.httpClient.put<Todo>(`https://boolean-uk-api-server.fly.dev/FredrikEH/todo/${updatedTodo.id}`, updatedTodo))
   }
 }
--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TodoService } from '../services/todo.service';
 import { Todo } from '../models/todo';
+import { filter, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-todo-list',
@@ -10,14 +11,25 @@ import { Todo } from '../models/todo';
 export class TodoListComponent {
   constructor(private readonly todoService: TodoService) {}
 
-  todos = this.todoService.todos;
+  todos = this.todoService.todos();
+
+  filteredTodos: Promise<Todo[]> = this.todos
+
+  // filterTodos() {
+  //   this.filteredTodos = this.todos?.pipe(
+  //     map((todos) =>
+  //       todos.filter((todo) => todo.completed === false)
+  //     )
+  //   );
+  // }
 
   updateTodo(todo: Todo) {
     this.todoService.updateTodo(todo);
+    //this.filterTodos()
   }
 
   async newTodo(title: string) {
     await this.todoService.addTodo(title);
-    this.todos = this.todoService.todos;
+    this.todos = this.todoService.todos();
   }
 }
