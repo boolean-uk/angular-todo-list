8fca52f102ecee7b8d351748134d54bcccce94c3
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-10-09 13:08:42 +0200

    Refactor app module and todo service
    - Add HttpClient provider to app module
    - Update todo service to use HttpClient for API requests
    - Replace Promise-based methods with Observable-based methods in todo service
    - Update todo list component to toggle between showing completed and uncompleted todos
    - Update todo list component template to display a button for toggling completed todos
    - Update todo list component to subscribe to updated todo events and refresh the todo list

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TodoService } from '../services/todo.service';
 import { Todo } from '../models/todo';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-todo-list',
@@ -8,16 +9,59 @@ import { Todo } from '../models/todo';
   styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent {
-  constructor(private readonly todoService: TodoService) {}
+  constructor(private readonly todoService: TodoService) {
+    this.updateTodoList();
+  }
+  showCompleted: boolean = false;
 
-  todos = this.todoService.todos;
+  filteredTodos: Observable<Todo[]> | undefined;
+  todos: Observable<Todo[]> | undefined;
 
-  updateTodo(todo: Todo) {
-    this.todoService.updateTodo(todo);
+  toggleShowCompleted() {
+    this.showCompleted = !this.showCompleted;
+    this.filterTodos();
   }
 
-  async newTodo(title: string) {
-    await this.todoService.addTodo(title);
-    this.todos = this.todoService.todos;
+  updateTodoList() {
+    this.todos = this.todoService.getAllTodos();
+    this.filterTodos();
+  }
+
+  filterTodos() {
+    this.filteredTodos = this.todos?.pipe(
+      map((todos) =>
+        todos.filter((todo) => todo.completed === this.showCompleted)
+      )
+    );
+  }
+
+  updateTodo(todo: Todo) {
+    this.todoService.updateTodo(todo).subscribe({
+      next: (response) => {
+        console.log('Car updated successfully', response);
+      },
+      error: (error) => {
+        console.error('Error updating car', error);
+      },
+      complete: () => {
+        console.log('Update car request completed');
+        this.updateTodoList();
+      },
+    });
+  }
+
+  newTodo(title: string) {
+    this.todoService.addTodo(title).subscribe({
+      next: (response) => {
+        console.log('Todo created successfully', response);
+      },
+      error: (error) => {
+        console.error('Error created todo', error);
+      },
+      complete: () => {
+        console.log('Update todo created completed');
+        this.updateTodoList();
+      },
+    });
   }
 }
