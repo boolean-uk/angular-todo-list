3482c55a54dda6480e629336dba180f603036272
Kaja Thoresen Plaszko <kaja.plaszko@no.experis.com>
2024-10-09 13:51:20 +0200

    Kaja Plaszko

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -8,16 +8,64 @@ import { Todo } from '../models/todo';
   styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent {
-  constructor(private readonly todoService: TodoService) {}
+  todos: Todo[] =  []
+  showCompleted: boolean = false;
+  todoToEdit: Todo | null = null;
 
-  todos = this.todoService.todos;
-
-  updateTodo(todo: Todo) {
-    this.todoService.updateTodo(todo);
+  constructor(private readonly todoService: TodoService) {
+    this.fetchTodos();
   }
 
-  async newTodo(title: string) {
-    await this.todoService.addTodo(title);
-    this.todos = this.todoService.todos;
+  fetchTodos() {
+    this.todoService.getTodos().subscribe((todos) => {
+      this.todos = todos
+    });
+  }
+
+  updateTodo(todo: Todo) {
+    this.todoService.updateTodo(todo).subscribe(() => {
+      this.fetchTodos();
+    });
+  }
+
+  newTodo(todo: Todo) {
+    if (todo.id) {
+      this.todoService.updateTodo({ ...todo, title: todo.title }).subscribe(() => {
+        this.fetchTodos();
+        this.todoToEdit = null;
+      });
+    } else {
+      this.todoService.addTodo(todo.title).subscribe(() => {
+        this.fetchTodos();
+      });
+    }
+  }
+
+  deleteTodo(todoId: number) {
+    this.todoService.deleteTodo(todoId).subscribe(() => {
+      this.todos = this.todos.filter(todo => todo.id !== todoId);
+    });
+
+    this.resetEditingState();
+  }
+
+  toggleShowCompleted() {
+    this.showCompleted = !this.showCompleted;
+  }
+
+  getFilteredTodos(): Todo[] {
+    if (this.showCompleted) {
+      return this.todos;
+    }
+
+    return this.todos.filter(todo => !todo.completed);
+  }
+
+  editTodo(todo: Todo) {
+    this.todoToEdit = todo;
+  }
+
+  resetEditingState() {
+    this.todoToEdit = null;
   }
 }
