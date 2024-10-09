53d8ed19940ab43dea0ae54770b50f74cdeacdbb
Oliver Andersson <oliverlundhandersson@gmail.com>
2024-10-09 11:09:04 +0200

    Done

--- src/app/todo/services/todo.service.ts
+++ src/app/todo/services/todo.service.ts
@@ -1,52 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Todo } from '../models/todo';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
-  private todoId = 1;
-  private todoList: Todo[] = [
-    {
-      id: this.todoId++,
-      title: 'serve the app',
-      completed: true,
-    },
-    {
-      id: this.todoId++,
-      title: 'familiarise yourself with the codebase',
-      completed: false,
-    },
-    {
-      id: this.todoId++,
-      title: 'start talking to the api',
-      completed: false,
-    },
-  ];
+  private apiUrl = 'https://boolean-uk-api-server.fly.dev/OliverLundhAndersson/todo';
 
-  // TODO replace with a get request
-  todos: Promise<Todo[]> = Promise.resolve(this.todoList);
+  constructor(private http: HttpClient) {}
 
-  async addTodo(title: string): Promise<Todo> {
-    // TODO: replace with a POST request
-    const todo = {
-      id: this.todoId++,
-      title: title,
-      completed: false,
-    };
-    this.todoList.push(todo);
-
-    return todo;
+  getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.apiUrl);
   }
 
-  async updateTodo(updatedTodo: Todo): Promise<Todo> {
-    // TODO: replace with a PUT request
-    const foundTodo = this.todoList.find((todo) => todo.id === updatedTodo.id);
-    if (!foundTodo) {
-      throw new Error('todo not found');
-    }
-    Object.assign(foundTodo, updatedTodo);
+  addTodo(title: string): Observable<Todo> {
+    const newTodo = { title, completed: false };
+    return this.http.post<Todo>(this.apiUrl, newTodo);
+  }
 
-    return foundTodo;
+  updateTodo(updatedTodo: Todo): Observable<Todo> {
+    const url = `${this.apiUrl}/${updatedTodo.id}`;
+    return this.http.put<Todo>(url, updatedTodo);
   }
 }
--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,23 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../services/todo.service';
 import { Todo } from '../models/todo';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css'],
 })
-export class TodoListComponent {
-  constructor(private readonly todoService: TodoService) {}
+export class TodoListComponent implements OnInit {
+  todos$: Observable<Todo[]> | null = null;
+  showCompleted = false;
 
-  todos = this.todoService.todos;
+  constructor(private todoService: TodoService) {}
 
-  updateTodo(todo: Todo) {
-    this.todoService.updateTodo(todo);
+  ngOnInit() {
+    this.fetchTodos();
+  }
+
+  fetchTodos() {
+    this.todos$ = this.todoService.getTodos();
+  }
+
+  filterTodos(todos: Todo[]): Todo[] {
+    return this.showCompleted ? todos : todos.filter(todo => !todo.completed);
+  }
+
+  toggleCompletedView() {
+    this.showCompleted = !this.showCompleted;
   }
 
   async newTodo(title: string) {
-    await this.todoService.addTodo(title);
-    this.todos = this.todoService.todos;
+    await this.todoService.addTodo(title).subscribe(() => this.fetchTodos());
+  }
+
+  async updateTodo(todo: Todo) {
+    await this.todoService.updateTodo(todo).subscribe(() => this.fetchTodos());
   }
 }
