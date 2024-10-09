05351ef8605f8ca298b55b5945f00baeb6057d94
bjorgastrid <bjorgkrist@hotmail.com>
2024-10-09 15:02:10 +0200

    core and extension, with some bugs

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TodoService } from '../services/todo.service';
 import { Todo } from '../models/todo';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-todo-list',
@@ -8,16 +9,34 @@ import { Todo } from '../models/todo';
   styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent {
-  constructor(private readonly todoService: TodoService) {}
+  onlyCompleted = true;
+  todos: Observable<Todo[]>
+  toDisplay : Todo[] = []
 
-  todos = this.todoService.todos;
+  constructor(private readonly todoService: TodoService) 
+  {
+    this.todos = this.todoService.getTodos();
+    this.changeDisplayed()
+  }
 
   updateTodo(todo: Todo) {
     this.todoService.updateTodo(todo);
   }
 
-  async newTodo(title: string) {
-    await this.todoService.addTodo(title);
-    this.todos = this.todoService.todos;
+  newTodo(title: string) {
+    this.todoService.addTodo({
+      id: 0,
+      title: title,
+      completed: false
+    });
+    this.todos = this.todoService.getTodos()
   }
+
+  changeDisplayed(){
+
+    this.onlyCompleted = !this.onlyCompleted;
+
+    const weirdIntermediate = this.todos.subscribe((todoArray: Todo[]) => this.toDisplay = todoArray.filter(t => t.completed === this.onlyCompleted)  )
+  }
+
 }
