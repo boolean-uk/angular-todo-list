30e2f5ef869185c94607590b7113046c1442e260
shyye <emma.art.dev@gmail.com>
2024-10-10 13:30:56 +0200

    Fix Extension

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TodoService } from '../services/todo.service';
 import { Todo } from '../models/todo';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-todo-list',
@@ -11,6 +12,8 @@ export class TodoListComponent {
   constructor(private readonly todoService: TodoService) {}
 
   todos = this.todoService.todos;
+  filteredTodos = this.getUncompleted()
+  isCompletedTasks = false
 
   updateTodo(todo: Todo) {
     this.todoService.updateTodo(todo);
@@ -19,5 +22,25 @@ export class TodoListComponent {
   async newTodo(title: string) {
     await this.todoService.addTodo(title);
     this.todos = this.todoService.todos;
+  }
+
+  getUncompleted() {
+    // return this.todos.map(todos => todos.filter(proj => proj.completed === true));
+    // TODO: read more about pipe()
+    return this.todos.pipe(map(todos => todos.filter(task => task.completed === false)))
+  }
+
+  getCompletedTasks() {
+    return this.todos.pipe(map(todos => todos.filter(task => task.completed === true)))
+  }
+
+  toggleCompletedTasks() {
+    this.isCompletedTasks = !this.isCompletedTasks
+
+    if (this.isCompletedTasks) {
+      this.filteredTodos = this.getCompletedTasks()
+    } else {
+      this.filteredTodos = this.getUncompleted()
+    }
   }
 }
