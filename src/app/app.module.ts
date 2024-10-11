b3d50a8bfd4fe0e65a7799b2864ba8412c33f3bc
Ludwig J Lundborg <ludwig.j.lundborg@gmail.com>
2024-10-11 15:16:52 +0200

    Extension finished: fixed small mistakes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { TodoModule } from './todo/todo.module';
 import { provideHttpClient } from '@angular/common/http';
-import { NgArrayPipesModule } from 'ngx-pipes';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, TodoModule],
-  providers: [provideHttpClient(), NgArrayPipesModule],
+  providers: [provideHttpClient()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
