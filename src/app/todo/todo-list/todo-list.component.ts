b95079c417333277143249e1db8f212236dfa5d6
Anders Hagen Ottersland <anderh0709@gmail.com>
2024-10-17 11:23:13 +0200

    show unfinished task chekcbox added (extension)

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TodoService } from '../services/todo.service';
 import { Todo } from '../models/todo';
-import { Observable } from 'rxjs';
+import { filter, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-todo-list',
@@ -11,13 +11,16 @@ import { Observable } from 'rxjs';
 export class TodoListComponent {
   todos$ = new Observable<Todo[]>();
 
+  @Input('checkbox') checkbox: boolean = false;
+
   ngOnInit(): void {
-    this.todos$ = this.todosService.todos$; 
+    this.todos$ = this.todosService.GetTodos(); 
     this.todos$.subscribe((todos) => {
       todos.forEach((todo) => {
         console.log(todo.title);
       });
     });
+    this.getFilteredTodos()
   }
 
   constructor(private readonly todosService: TodoService) {}
@@ -26,10 +29,29 @@ export class TodoListComponent {
     this.todosService.updateTodo(todo);
   }
 
-
   newTodo(title: string) {
     this.todosService.addTodo(title);
+    this.getFilteredTodos();
+  }
+
+  toggleTaskList() {
+    this.checkbox = !this.checkbox;
+    if(!this.checkbox){
+      this.todos$ = this.todos$.pipe(map(todos => {
+        return todos.filter(todo => todo.completed)
+      }))
+      return;
+    }
     this.todos$ = this.todosService.GetTodos();
   }
   
+  getFilteredTodos() {
+    if(!this.checkbox){
+      this.todos$ = this.todos$.pipe(map(todos => {
+        return todos.filter(todo => todo.completed)
+      }))
+      return;
+    }
+    this.todos$ = this.todosService.GetTodos();
+  }
 }
