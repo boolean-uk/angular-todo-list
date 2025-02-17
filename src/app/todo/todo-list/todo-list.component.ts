913ba5b5ab6a84c33f12ae181c544f743b6291e1
Nikolai Lillemark Børseth <nikolailb1999@gmail.com>
2025-02-17 10:07:54 +0100

    Completed core + ext.

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TodoService } from '../services/todo.service';
 import { Todo } from '../models/todo';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-todo-list',
@@ -8,16 +9,79 @@ import { Todo } from '../models/todo';
   styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent {
-  constructor(private readonly todoService: TodoService) {}
+  constructor(private readonly todoService: TodoService) {
+    this.loadTodos();
+  }
 
-  todos = this.todoService.todos;
+  todos: Todo[] = [];
+  filtered: Todo[] = [];
+  isCompleted: boolean = false;
+  isLoading: boolean = false;
+  errorMessage: string = '';
 
-  updateTodo(todo: Todo) {
-    this.todoService.updateTodo(todo);
+  async updateTodo(todo: Todo) {
+    this.todoService.updateTodo(todo).subscribe({
+      next: (response) => {
+        console.log('Updated todo:', response);
+        this.loadTodos();
+      },
+    });
   }
 
   async newTodo(title: string) {
-    await this.todoService.addTodo(title);
-    this.todos = this.todoService.todos;
+    this.todoService.addTodo(title).subscribe({
+      next: (response) => {
+        console.log('Created todo: ', response);
+        this.loadTodos();
+      },
+      error: (error) => {
+        console.log('Failed to create todo:', error);
+        this.errorMessage = error;
+      },
+    });
   }
+
+  toggleIsCompleted(): void {
+    this.filter();
+  }
+
+  loadTodos(): void {
+    this.isLoading = true;
+    this.todoService.getTodos().subscribe({
+      next: (response) => {
+        this.todos = response;
+        this.filter();
+      },
+      error: (error) => {
+        this.errorMessage = error;
+        this.isLoading = false;
+      },
+      complete: () => {
+        this.isLoading = false;
+      },
+    });
+  }
+
+  filter(): void {
+    this.filtered = this.todos.filter(
+      (todo) => todo.completed === this.isCompleted
+    );
+  }
+  // filter(): void {
+  //   this.isLoading = true;
+  //   this.todoService.getTodos().subscribe({
+  //     next: (response) => {
+  //       this.filtered = response.filter(
+  //         (todo) => todo.completed === this.isCompleted
+  //       );
+  //     },
+  //     error: (error) => {
+  //       this.errorMessage = error;
+  //       this.isLoading = false;
+  //     },
+  //     complete: () => {
+  //       this.isLoading = false;
+  //     },
+  //   });
+  // }
 }
