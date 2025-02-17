dccda942b0c7257f08688b05ac0339acd616e3e6
Kristoffer Blucher <kristofferblucher@gmail.com>
2025-02-17 13:33:22 +0100

    Core done

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,23 +1,36 @@
 import { Component } from '@angular/core';
 import { TodoService } from '../services/todo.service';
 import { Todo } from '../models/todo';
+import { Observable } from 'rxjs';
+import { Injectable, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css'],
 })
-export class TodoListComponent {
+export class TodoListComponent  {
+  
+  todos = this.todoService.getTodos()
+
   constructor(private readonly todoService: TodoService) {}
+  
 
-  todos = this.todoService.todos;
-
-  updateTodo(todo: Todo) {
-    this.todoService.updateTodo(todo);
+  updateTodo(todo: Todo): void {
+    this.todoService.updateTodo(todo).subscribe(() => {
+      this.todos = this.todoService.getTodos()
+    });
   }
 
-  async newTodo(title: string) {
-    await this.todoService.addTodo(title);
-    this.todos = this.todoService.todos;
+  newTodo(title: string) {
+  this.todoService.addTodo(title).subscribe(() => {
+    this.todos = this.todoService.getTodos()
+  });
   }
+
+
+  // toggleDisplay(): void {
+  //   this.showCompleted = !this.showCompleted
+
+  
 }
