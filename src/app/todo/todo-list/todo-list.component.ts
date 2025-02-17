42a0d4d8fceddad93e02b7412ab4348e34045aea
abdi <abdi060902@outlook.com>
2025-02-17 13:46:16 +0100

    core

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,23 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../services/todo.service';
 import { Todo } from '../models/todo';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css'],
 })
-export class TodoListComponent {
-  constructor(private readonly todoService: TodoService) {}
+export class TodoListComponent implements OnInit{
+  todos$ = new Observable<Todo[]>();
+  constructor(private readonly todoService: TodoService) {
+    console.log('url:', `${environment.apiUrl}`);
+  }
+  ngOnInit(): void {
+    this.todos$ = this.todoService.getTodos();
+    this.todos$.subscribe((todos) => {
+      todos.forEach((todos) => {
+        console.log(todos.title);
+      });
+    });
+  }
 
-  todos = this.todoService.todos;
+  
 
   updateTodo(todo: Todo) {
     this.todoService.updateTodo(todo);
+    
+
   }
 
   async newTodo(title: string) {
     await this.todoService.addTodo(title);
-    this.todos = this.todoService.todos;
+    this.todos$ = await this.todoService.getTodos();
   }
 }
